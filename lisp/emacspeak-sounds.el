fa80775f849b81eabccb8a862c2e501d0424b811
Igor B. Poretsky <poretsky@mlbox.ru>
2006-09-03 07:46:33 +0000

    Force home directory as a default directory when playing sound to make it possible even when current default directory doesn't exist. (This patch was somehow lost in version 24.)

--- lisp/emacspeak-sounds.el
+++ lisp/emacspeak-sounds.el
@@ -316,7 +316,8 @@ Sound is produced only if `emacspeak-use-auditory-icons' is true.
 See command `emacspeak-toggle-auditory-icons' bound to \\[emacspeak-toggle-auditory-icons ]."
   (declare (special  emacspeak-use-auditory-icons emacspeak-play-program))
   (and emacspeak-use-auditory-icons
-       (let ((process-connection-type nil))
+       (let ((process-connection-type nil)
+	     (default-directory (file-name-as-directory (getenv "HOME"))))
 		 (condition-case err
 			 (start-process
 			  "play" nil emacspeak-play-program
