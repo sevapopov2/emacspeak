6d99514e47ef1f9af0533030e061147a748ea754
Igor B. Poretsky <poretsky@mlbox.ru>
2010-01-29 22:12:14 +0000

    Removed references to the obsolete atom-blogger. Mention g-client in info finder.

--- lisp/emacspeak-finder-inf.el
+++ lisp/emacspeak-finder-inf.el
@@ -614,10 +614,67 @@
     ("xml-parse.el"
         "code to efficiently read/write XML data with Elisp"
         (convenience languages lisp xml parse data))
+    ("g-app.el"
+        "Google Client APP methods"
+        (google   atom api  google services))
+    ("g-auth.el"
+        "Google Authentication Module"
+        (google   auth))
+    ("g-autogen.el"
+        "Generate autoloads for G"
+        (google services))
+    ("g-cus-load.el"
+        "automatically extracted custom dependencies"
+        nil)
+    ("g-load-path.el"
+        "Setup Emacs load-path for compiling grep"
+        (g-client  google services  for emacs))
+    ("g-loaddefs.el"
+        nil
+        nil)
+    ("g-utils.el"
+        "Google Client Utilities"
+        (google   atom api  google services))
+    ("g.el"
+        "Google Client"
+        (emacs g-client  google   atom api  google services))
+    ("gblogger.el"
+        "new Atom Blogger API client"
+        (g-client  blogger atom api))
+    ("gcal.el"
+        "Google Calendar"
+        (google   atom api))
+    ("gnotebook.el"
+        "Google Notebook"
+        (google   services))
+    ("gphoto.el"
+        "Google  Picasa Client"
+        (google   atom api))
+    ("greader.el"
+        "Google Reader"
+        (google   atom api))
+    ("gsheet.el"
+        "Google Spreadsheets"
+        (spreadsheets  gdata  google   atom api))
+    ("gskeleton.el"
+        "Skeleton Google  Client"
+        (google   atom api))
+    ("gtube.el"
+        "YouTube Google  Client"
+        (google    api))
+    ("indent-files.el"
+        nil
+        nil)
+    ("json.el"
+        "JavaScript Object Notation parser / generator"
+        (convenience))
 ))
 
-(loop for l  in (reverse emacspeak-finder-package-info) do
- (push l finder-package-info))
 (provide 'emacspeak-finder-inf)
 
+;;; Local Variables:
+;;; version-control: never
+;;; no-byte-compile: t
+;;; no-update-autoloads: t
+;;; End:
 ;;; emacspeak-finder-inf.el ends here
