425a11ab8d525c21b8b80d50b371c96bb77c1ce5
Igor B. Poretsky <poretsky@mlbox.ru>
2013-12-25 15:59:46 +0000

    Merged last upstream fixes.

--- lisp/emacspeak-load-path.el
+++ lisp/emacspeak-load-path.el
@@ -71,4 +71,26 @@
     (defalias 'ems-print-help-return-message 'help-print-return-message)
   (defalias 'ems-print-help-return-message 'print-help-return-message))
 
+(unless (fboundp 'with-silent-modifications)
+  (defmacro with-silent-modifications (&rest body)
+    "Execute BODY, pretending it does not modify the buffer.
+If BODY performs real modifications to the buffer's text, other
+than cosmetic ones, undo data may become corrupted.
+Typically used around modifications of text-properties which do not really
+affect the buffer's content."
+    (declare (debug t) (indent 0))
+    (let ((modified (make-symbol "modified")))
+      `(let* ((,modified (buffer-modified-p))
+              (buffer-undo-list t)
+              (inhibit-read-only t)
+              (inhibit-modification-hooks t)
+              deactivate-mark
+              buffer-file-name
+              buffer-file-truename)
+         (unwind-protect
+             (progn
+               ,@body)
+           (unless ,modified
+             (restore-buffer-modified-p nil)))))))
+
 (provide 'emacspeak-load-path)
