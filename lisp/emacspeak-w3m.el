998c2eebf460d24277047c2a92467fd03218c925
Igor B. Poretsky <poretsky@mlbox.ru>
2014-12-18 20:31:40 +0000

    Links with title attribute but without enclosed text are made visible.

--- lisp/emacspeak-w3m.el
+++ lisp/emacspeak-w3m.el
@@ -720,6 +720,9 @@ Indicate change of selection with
 
 (defadvice w3m-decode-buffer (before emacspeak pre act comp)
   "Apply requested transform if any before displaying the HTML. "
+  (goto-char (point-min))
+  (while (re-search-forward "<a[[:blank:]][^>]*?[[:blank:]]title=\"\\([^\"]*\\)\"[^>]*?>\\([[:blank:]]*?\\)</a>" nil t)
+    (replace-match (match-string 1) t t nil 2))
   (when emacspeak-w3m-text-input-field-types
     (goto-char (point-min))
     (while (re-search-forward (format "<input[^>]*?[[:blank:]]type=\"\\(%s\\)\""
