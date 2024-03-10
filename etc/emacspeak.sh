e713d668099b716f6b86ffdd30ee996192aff509
Vsevolod Popov <sevapopov13@gmail.com>
2024-03-10 16:19:11 +0300

    Patch 118.

--- etc/emacspeak.sh
+++ etc/emacspeak.sh
@@ -1,41 +1,37 @@
 #!/bin/sh
 # emacspeak - execute emacs with speech enhancements
-# this file needs a maintainer, I, (tv.raman.tv@gmail.com) am not presently maintaining it.
-# AKA  Use at your own risk, and if it breaks, you get to keep both pieces. 
-# Patches that are fully tested always welcome.
-#$Id$
-    if [ -f /etc/emacspeak.conf ]
-    then
-    . /etc/emacspeak.conf
-fi
+#
+# WARNING:
+# This file is not the original one:
+# it is modified for Debian speech support.
 
-if [ -f $HOME/.emacs ]
-then
-	INITSTR="-l $HOME/.emacs"
+if [ -f /etc/emacspeak.conf ]
+then . /etc/emacspeak.conf
 fi
 
 CL_ALL=""
 for CL in $* ; do
-	if [ "$CL" = "-o" ]; then
-		DTK_PROGRAM=stereo-outloud
-		export DTK_PROGRAM
-	elif [ "$CL" = "-m" ]; then
-		DTK_PROGRAM=multispeech
-		export DTK_PROGRAM
-	elif [ "$CL" = "-e" ]; then
-		DTK_PROGRAM=espeak
-		export DTK_PROGRAM
-	elif [ "$CL" = "-d" ]; then
-		DTK_PROGRAM=dtk-soft
-		export DTK_PROGRAM
-	elif [ "$CL" = "-q" ]; then
-		INITSTR=""
-	else
-		CL_ALL="$CL_ALL $CL"
-	fi
+    case "$CL" in
+        -o) DTK_PROGRAM=stereo-outloud
+            ;;
+        -m) DTK_PROGRAM=multispeech
+            ;;
+        -e) DTK_PROGRAM=espeak
+            ;;
+        -d) DTK_PROGRAM=dtk-exp
+            ;;
+        *) CL_ALL="$CL_ALL $CL"
+            ;;
+    esac
 done
+export DTK_PROGRAM
 
+# Once we use Emacspeak as an all-purpose shell:
+EDITOR="emacsclient --alternate-editor emacspeak"
+VISUAL=$EDITOR
+export EDITOR
+export VISUAL
 
-EMACS_UNIBYTE=1
-export EMACS_UNIBYTE
-exec emacs -q -l /lisp/emacspeak-setup.el $INITSTR $CL_ALL
+EMACSPEAK=yes
+export EMACSPEAK
+exec emacs $CL_ALL
