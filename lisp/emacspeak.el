35797e6afec84f00d3bd4d55aab3d7af86a80a2c
Vsevolod Popov <sevapopov13@gmail.com>
2024-03-10 15:38:39 +0300

    Patch 52.

--- lisp/emacspeak.el
+++ lisp/emacspeak.el
@@ -328,6 +328,7 @@ that implements the speech-enabling extensions for `package' (a string)."
 (emacspeak-do-package-setup "view-pr" 'emacspeak-view-process)
 (emacspeak-do-package-setup "vm" 'emacspeak-vm)
 (emacspeak-do-package-setup "w3m" 'emacspeak-w3m)
+(emacspeak-do-package-setup "wget" 'emacspeak-wget)
 (emacspeak-do-package-setup "wdired" 'emacspeak-wdired)
 (emacspeak-do-package-setup "cus-edit" 'emacspeak-custom)
 (emacspeak-do-package-setup "wid-edit" 'emacspeak-widget)
